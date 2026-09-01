c3d06feec26fc02616f4ed412ce908e352611885
Ulasoglu <m1p@web.de>
2026-09-02 00:12:21 +0200

    chore: refresh mobile start-state cache

--- web/sw.js
+++ web/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "sehrin-ne-oldu-v10-integrated-filters";
+const CACHE = "sehrin-ne-oldu-v11-mobile-start-state";
 const SHELL = ["./", "./index.html", "./styles.css", "./mobile-tune.css", "./image-cards.css", "./pulse-experience.css", "./app.js", "./pulse-experience.js", "./weather-ui.js", "./image-cards.js", "./manifest.webmanifest"];
 
 self.addEventListener("install", (event) => {
